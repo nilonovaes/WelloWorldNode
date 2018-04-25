546d21a1ca6db92253f45578b03bad79925acd94
Danilo Novaes <danilo.novaes@live.com>
2018-04-25 19:55:17 -0300

    Teste02

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var app = require('./config/express')();
 
-
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
--- config/Express.js
+++ config/Express.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var consign = require('consign');
+ var consign = require('consign');
 var bodyParser = require('body-parser');
 var validator = require('express-validator');
 
